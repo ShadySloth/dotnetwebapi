8e88ab447546f7b14198c68fef4fbdc7ce5f27f8
ShadySloth <pedant01@easv365.dk>
2023-08-16 11:23:38 +0200

    asdasd

---
+++ Controllers/HelloController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace dotnetwebapi.Controllers;
+
+[ApiController]
+
+public class HelloController : ControllerBase
+{
+    
+    [HttpGet]
+    [Route("/{id}")]
+    public String get([FromRoute] int id, [FromQuery]string someValue, [FromHeader]string someValue)
+    {
+        return "Hello " + id + " :) Some value:" + someValue + "Header: ";
+    }
+}
